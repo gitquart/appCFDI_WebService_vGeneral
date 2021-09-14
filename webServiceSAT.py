67b5c29de223efd5c1dd33bc184e43f50bca2846
Ulyses <ulyses.rico.2012@gmail.com>
2021-09-14 10:43:13 -0500

    ...

--- webServiceSAT.py
+++ webServiceSAT.py
@@ -1,1 +1,7 @@
-https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/Autenticacion.svc
+from suds.client import Client
+
+url='https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/Autenticacion/Autenticacion.svc?wsdl'
+client=Client(url)
+print(client)
+
+
